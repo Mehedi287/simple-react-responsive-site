57f4df29fcaf9fed1373e3b6eee1cdea11d5a9d2
Mehedi390 <mh0816150@gmail.com>
2021-10-12 23:09:34 -0700

    add expart pat

--- src/components/OurExpart/OurExpert.js
+++ src/components/OurExpart/OurExpert.js
@@ -3,40 +3,32 @@ import React from 'react';
 const OurExpert = () => {
     return (
         <>
-            <h2>Our Expert </h2>
-            <div className="col-md-6 col-sm-12 col-4">
-                <div className="">
-                    <img src="https://image.freepik.com/free-photo/mechanic-preparing-check-list_1170-1642.jpg" alt="" />
-                    <h3>MR.Jhoan</h3>
+            <h2 className="m-5 text-success">Our Expert </h2>
+            <div className="row mb-5">
+                <div className="col-lg-4 col-md-6 col-sm-12 b-rounded-md">
+                    <img className="img-fluid w-50 m-4" src="https://image.freepik.com/free-photo/car-mechanic-wearing-white-uniform-stand-holding-wrench_1150-16608.jpg" alt="" />
+                    <h5>MR.Jhoan</h5>
                 </div>
-                <div className="">
-                    <img src="https://image.freepik.com/free-photo/car-mechanic-wearing-white-uniform-stand-holding-wrench_1150-16605.jpg" alt="" />
-                    <h3>Mical krud</h3>
+                <div className="col-lg-4 col-md-6 col-sm-12 rounded-lg">
+                    <img className="img-fluid w-50 " src="https://image.freepik.com/free-photo/car-mechanic-wearing-white-uniform-stand-holding-wrench_1150-16605.jpg" alt="" />
+                    <h5>Mical krud</h5>
                 </div>
-                <div className="">
-                    <img src="https://image.freepik.com/free-photo/car-joy-smiling-spanner-occupation_1134-982.jpg" alt="" />
-                    <h3>Jams kruna</h3>
+                <div className="col-lg-4 col-md-6 col-sm-12 rounded-lg">
+                    <img className="img-fluid w-50 " src="https://image.freepik.com/free-photo/car-joy-smiling-spanner-occupation_1134-982.jpg" alt="" />
+                    <h5>Jams kruna</h5>
                 </div>
-                <div className="">
-                    <img src="https://image.freepik.com/free-photo/mechanic-changing-tires-car-service_1303-26888.jpg" alt="" />
-                    <h3>Batlar </h3>
+                <div className="col-lg-4 col-md-6 col-sm-12 rounded-lg">
+                    <img className="img-fluid w-50 " src="https://image.freepik.com/free-photo/mechanic-changing-tires-car-service_1303-26888.jpg" alt="" />
+                    <h5>Batlar </h5>
                 </div>
-                <div className="">
-                    <img src="https://image.freepik.com/free-photo/portrait-beautiful-woman-engineer_23-2148826527.jpg" alt="" />
-                    <h3>Mandalea</h3>
+                <div className="col-lg-4 col-md-6 col-sm-12 rounded-lg">
+                    <img className="img-fluid w-50 " src="https://image.freepik.com/free-photo/portrait-beautiful-woman-engineer_23-2148826527.jpg" alt="" />
+                    <h5>Mandalea</h5>
                 </div>
-                <div className="">
-                    <img src="https://image.freepik.com/free-photo/handsome-elegant-man-car-salon_1157-22330.jpg" alt="" />
-                    <h3>Morgan</h3>
+                <div className="col-lg-4 col-md-6 col-sm-12 rounded-lg">
+                    <img className="img-fluid w-50 " src="https://image.freepik.com/free-photo/handsome-elegant-man-car-salon_1157-22330.jpg" alt="" />
+                    <h5>Morgan</h5>
                 </div>
-
-
-
-
-
-
-
-
             </div>
         </>
     );
